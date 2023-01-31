86d41a21ca7d6a53fec112ffd60b0fdc76d91685
ByungJun Kang <44726494+BangDori@users.noreply.github.com>
2023-02-01 01:21:33 +0900

    code cleanup & remark

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,14 +1,26 @@
+/****************
+ todos 배열을 props로 받아 온 후, 이를 배열 내장 함수 map을 사용해서 여러 개의
+ TodoListItem 컴포넌트로 변환하여 보여 줍니다.
+ *****************/
+
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-    return (
-        <div classname="TodoList">
-            {todos.map(todo => {
-                return <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}/>
-            })}
-        </div>
-    );
+  return (
+    <div className="TodoList">
+      {todos.map((todo) => {
+        return (
+          <TodoListItem
+            todo={todo}
+            key={todo.id}
+            onRemove={onRemove}
+            onToggle={onToggle}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default TodoList;
--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -1,25 +1,30 @@
+/****************
+ 각 할 일 항목에 대한 정보를 보여 주는 컴포넌트입니다. todo 객체를 props로
+ 받아 와서 상태에 다라 다른 스타일의 UI를 보여 줍니다.
+ *****************/
+
 import {
-    MdCheckBoxOutlineBlank,
-    MdCheckBox,
-    MdRemoveCircleOutline,
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
 } from 'react-icons/md';
 import cn from 'classnames';
 import './TodoListItem.scss';
 
 const TodoListItem = ({ todo, onRemove, onToggle }) => {
-    const { id, text, checked } = todo;
+  const { id, text, checked } = todo;
 
-    return (
-        <div className='TodoListItem'>
-            <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-                <div className="text">{ text }</div>
-            </div>
-            <div className='remove' onClick={() => onRemove(id)}>
-                <MdRemoveCircleOutline />
-            </div>
-        </div>
-    );
+  return (
+    <div className="TodoListItem">
+      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
+      </div>
+      <div className="remove" onClick={() => onRemove(id)}>
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
+  );
 };
 
 export default TodoListItem;
