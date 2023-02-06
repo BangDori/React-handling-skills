9c3fc3324144c8d40e1cc5a2704183378c590c18
BangDori <qudwns8865@naver.com>
2023-02-07 00:30:26 +0900

    Add: comment

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -8,9 +8,19 @@ import rootReducer from './modules';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+/**
+ * Store 생성
+ * rootReducer (Reducer function)
+ * composeWithDevTools() 개발자 도구 추가
+ */
 const store = legacy_createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+/**
+ * 리액트 컴포넌트에서 스토어를 사용할 수 있도록 하기 위해
+ * App 컴포넌트를 react-redux에서 제공하는 Provider 컴포넌트로 감싸기
+ */
 root.render(
   <Provider store={store}>
     <App />
