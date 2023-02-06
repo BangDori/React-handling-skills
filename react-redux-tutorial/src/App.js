7568d3fa67a709b2bc981285b8f5afc6645bff94
BangDori <qudwns8865@naver.com>
2023-02-07 00:22:27 +0900

    Add: Containers

---
+++ react-redux-tutorial/src/App.js
@@ -0,0 +1,14 @@
+import TodosContainer from './containers/TodosContainer';
+import CounterContainer from './containers/CounterContainer';
+
+const App = () => {
+  return (
+    <>
+      <CounterContainer />
+      <hr />
+      <TodosContainer />
+    </>
+  );
+};
+
+export default App;
