be34f3916d08194ef9852e6ecbbd0e6abd91a1e3
BangDori <qudwns8865@naver.com>
2023-02-07 00:17:45 +0900

    Add: store, provider, devTool

---
+++ react-redux-tutorial/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { legacy_createStore } from 'redux';
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = legacy_createStore(rootReducer, composeWithDevTools());
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
+
+reportWebVitals();
