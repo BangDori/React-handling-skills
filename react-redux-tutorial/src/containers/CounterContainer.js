39cd0989d45a518483d17458f2fd71fca08a9169
ByungJun Kang <44726494+BangDori@users.noreply.github.com>
2023-02-07 21:45:26 +0900

    Add coment & remove connect

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,11 +1,19 @@
 import { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { connect } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
+// Counter에 대한 기능 처리 담당
 const CounterContainer = () => {
+  /**
+   * useSelector hook은 스토어의 상태를 반환해주는 역할
+   * useSelector를 사용한 함수에서 리덕스 스토어의 상태값이 바뀐 경우
+   * 바뀐 스토어의 상태값을 다시 가져와 컴포넌트를 렌더링 시킨다.
+   * 컴포넌트 렌더링 최적화에 용이
+   */
   const number = useSelector((state) => state.counter.number);
+
+  // store에 dispatch하기 위해 함수형 컴포넌트에서 useDispatch hook을 사용
   const dispatch = useDispatch();
   const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
   const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
@@ -15,12 +23,4 @@ const CounterContainer = () => {
   );
 };
 
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  },
-)(CounterContainer);
+export default CounterContainer;
