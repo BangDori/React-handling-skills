376f31802c5aeaeae931c6346acc7b800f37a61c
BangDori <qudwns8865@naver.com>
2023-02-08 20:05:53 +0900

    update middleware

--- learn-redux-middleware/src/containers/CounterContainer.js
+++ learn-redux-middleware/src/containers/CounterContainer.js
@@ -1,14 +1,22 @@
-import { increase, decrease } from "../modules/counter";
+import { increaseAsync, decreaseAsync } from "../modules/counter";
+// import { increase, decrease } from "../modules/counter";
 import Counter from "../components/Counter";
 import { useSelector, useDispatch } from "react-redux";
-import { useCallback } from "react";
+// import { useCallback } from "react";
 
 const CounterContainer = () => {
-  const number = useSelector((state) => state.counter.number);
+  const number = useSelector((state) => state.counter);
 
   const dispatch = useDispatch();
-  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
-  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
+  const onIncrease = () => {
+    dispatch(increaseAsync());
+  };
+  const onDecrease = () => {
+    dispatch(decreaseAsync());
+  };
+  // const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  // const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
 
   return (
     <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
